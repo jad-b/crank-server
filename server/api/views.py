2a5f3508739b9b4ab1460894a09e8b264632ad20
jdobbinsbucklad <jdobbinsbucklad@dyn.com>
2015-01-29 10:47:54 -0500

    Uncomment login()

--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+# from django.shortcuts import render
+# from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.views.generic import View
 
-# Create your views here.
+
 class AuthenticateView(View):
     """Authenticate a user."""
 
@@ -14,17 +14,16 @@ class AuthenticateView(View):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                # login(request, user)
+                login(request, user)
                 # Redirect to a success page.
                 return JsonResponse({'message':
                                      'You have successfully authenticated',
                                      'error': False})
             else:
                 # Return a 'disabled account' error message
-               return JsonResponse({'message': 'Your account is not active',
+                return JsonResponse({'message': 'Your account is not active',
                                     'error': True})
         else:
             # Return an 'invalid login' error message.
             return JsonResponse({'message': 'Credentials are invalid',
                                  'error': True})
-
