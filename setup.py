8b8594a81d98ed02ee6f7ebcde804aa584356a1e
jdobbinsbucklad <jdobbinsbucklad@dyn.com>
2015-03-03 21:44:07 -0500

    Updating project metadata

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
 import os
 from setuptools import setup
 
+
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+def read_version(filename='version'):
+    with open(filename) as f:
+        return f.read().strip()
 
 setup(
     name='crank-server',
-    version='0.1',
+    version=read_version(),
     packages=['crank'],
     include_package_data=True,
-    license='MIT License',  # example license
+    license='MIT License',
     description='API server for Crank interactions',
     long_description=README,
     url='http://www.github.com/jad-b/crank-server',
     author='Jeremy Dobbins-Bucklad',
-    author_email='j.american.db@example.com',
+    author_email='j.american.db@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',   # example license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
