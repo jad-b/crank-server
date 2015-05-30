4e0bcecafa2d5d25460c7493b73ac0a8b54615af
jdobbinsbucklad <jdobbinsbucklad@gmail.com>
2015-05-29 20:57:21 -0400

    Make room for Go implementation

--- bin/test_changes.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Detected changes in these files: $@"
-
-echo "Detecting associated tests..." 
-
-exit 0
--- server/server/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class HomeView(TemplateView):
-    template_name = 'server/base.html'
