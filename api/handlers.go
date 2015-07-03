61cc5f2b5dbaa0a4252abcdb15163a439ef2619b
jdobbinsbucklad <jdobbinsbucklad@gmail.com>
2015-07-03 14:50:58 -0400

    Client and server code for POSTing bodyweight

--- api/handlers.go
+++ api/handlers.go
@@ -1,12 +1,22 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/jad-b/crank/crank"
 )
+
+// BodyweightRecord encapsulates a record about bodyweight.
+type BodyweightRecord struct {
+	Bodyweight float32 `json:"bodyweight"`
+	// 'omitempty' => Skip the timestamp if it's empty
+	Timestamp time.Time `json:"timestamp"`
+}
 
 // GetWorkoutHandler returns a workout by timestamp
 func GetWorkoutHandler(w http.ResponseWriter, req *http.Request) {
@@ -17,6 +27,22 @@ func GetWorkoutHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	writeJSON(w, http.StatusOK, workout)
+}
+
+// PostBodyweightHandler creates a new bodyweight record.
+func PostBodyweightHandler(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+	}
+	bwRec := &BodyweightRecord{}
+	err = json.Unmarshal(body, bwRec)
+	if err != nil {
+		http.Error(w, "Failed to parse JSON from request", http.StatusBadRequest)
+	}
+	// TODO Create a record in the database
+	fmt.Println("Received a bodyweight of %f w/ timestamp %s",
+		bwRec.Bodyweight, bwRec.Timestamp)
 }
 
 func timeFromQuery(req *http.Request) (t time.Time, err error) {
