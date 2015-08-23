63bdfcbf8be005cb65e830135d63debb075f3143
moogar0880 <moogar0880@gmail.com>
2015-08-22 22:17:31 -0400

    Adding new fields to ExerciseMeta struct

--- training/workout.go
+++ training/workout.go
@@ -24,10 +24,84 @@ type Exercise struct {
 	Comment string `json:"comment"`
 }
 
-// ExerciseTag ...
-type ExerciseTag struct {
-	Tag     string `json:"tag"`
-	Primary bool   `json:"primary"`
+/*
+MUSCLE_GROUPS = (('Q', 'Quadriceps'),
+                 ('LB', 'Lower Back'),
+                 ('B', 'Biceps'),
+                 ('C', 'Chest'),
+                 ('A', 'Abdominals'),
+                 ('H', 'Hamstrings'),
+                 ('T', 'Triceps'),
+                 ('TR', 'Traps'),
+                 ('M', 'Middle Back'),
+                 ('L', 'Lats'),
+                 ('N', 'Neck'),
+                 ('F', 'Forearms'),
+                 ('G', 'Glutes'),
+                 ('S', 'Shoulders'),
+                 ('CA', 'Calves'))
+*/
+
+// Collection of valid values for an assortment of ExerciseMeta fields. Note
+// that these can not be marked as constant, because Array's are mutable.
+// Because of this avoid, at all costs, ever writing to any of these Arrays
+var (
+	ExerciseCategories = [3]string{"Resistance", "Functional", "Cardio"}
+	MuscleGroups       = [15]string{"Quadriceps", "Lower Back", "Biceps", "Chest",
+		"Abdominals", "Hamstrings", "Triceps", "Traps",
+		"Middle Back", "Lats", "Neck", "Forearms",
+		"Glutes", "Shoulders", "Calves"}
+	ExerciseMechanics = [2]string{"Compound", "Isolation"}
+	DifficultyLevels  = [3]string{"Beginner", "Intermediate", "Expert"}
+	Forces            = [3]string{"Push", "Pull", "Static"}
+	ExerciseTypes     = [7]string{"Cardio", "Olympic Weightlifting", "Plyometrics",
+		"Powerlifting", "Strength", "Stretching",
+		"Strongman"}
+)
+
+// ExerciseMeta is used to encapsulate the metadata of a specific exercise.
+type ExerciseMeta struct {
+	// The name of this exercise
+	Name string `json:"name"`
+
+	// The rate at which weight/reps for this excercise should be increased by
+	ProgressDifferential float64 `json:"progress_differential"`
+
+	// Primary muscle group impacted by this exercise
+	PrimaryMuscleGroup string `json:"primary_muscle_group"`
+
+	// Secondary muscle group impacted by this exercise
+	SecondaryMuscleGroup string `json:"secondary_muscle_group"`
+
+	// What "type" of exercise is this?
+	Category string `json:"category"`
+
+	// Aliases for this Exercise
+	AlsoKnownAs string `json:"also_known_as"`
+
+	// One of Compound or Isolation, describing the mechanics of this exercise
+	MechanicsType string `json:"mechanics_type"`
+
+	// A link to a heatmap image of the muscles impacted by this exercise
+	MuscleImage string `json:"muscle_image"`
+
+	// A newline separated textual guide to this exercise
+	Guide string `json:"guide"`
+
+	// One of Beginner, Intermediate, Expert. Indicating the level of skill
+	// required to perform this exercise successfully
+	DifficultyLevel string `json:"difficulty_level"`
+
+	// Free form text describing any equipment required to perform this
+	// exercise
+	Equipment string `json:"equipment"`
+
+	// The direction of force applied to perform this exercise. Will be one of
+	// Push, Pull, Static, or None
+	Force string `json:"force"`
+
+	// This Exercise's type
+	ExerciseType string `json:"type"`
 }
 
 // Set ...
