ed6f4e7d5f7d305a07e7f0f2e6a93c515a749d33
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2015-09-28 06:27:46 -0400

    Comment timestamp test for workflow

--- web_test.go
+++ web_test.go
@@ -10,27 +10,32 @@ import (
 )
 
 func TestTimestampInRequests(t *testing.T) {
+	// Test setup
 	type StampStruct struct {
 		Timestamp time.Time
 	}
-	// Test request handler
+	// 3) Out-of-order; see below:
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		//   a) Server parses timestamp from most-to-least precise
 		ts, err := GetTimestampQuery(r)
 		if err != nil {
 			HTTPError(w, err, http.StatusBadRequest)
 			return
 		}
+		// b) DB op(s) go here...
 		t.Logf("Received: %s", ts.String())
 		WriteOkayJSON(w, StampStruct{Timestamp: ts})
 	}
 	resp := httptest.NewRecorder()
-	// setup timestamp in request
+
+	// 1) Client prepares request
 	stamp := time.Now().UTC()
 	u := &url.URL{
 		Scheme: "http",
 		Host:   "localhost",
 		Path:   "/timestamp",
 	}
+	// 1a) Client sets timestamp to appropriate precision
 	SetTimestampQuery(u, stamp)
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
@@ -38,8 +43,10 @@ func TestTimestampInRequests(t *testing.T) {
 	}
 	t.Logf("Query string: %s", req.URL.RawQuery)
 
+	// 2) Request is sent to server
 	handler(resp, req)
 
+	// Assertions
 	if resp.Code != 200 {
 		t.Fatal(resp.Body.String())
 	}
